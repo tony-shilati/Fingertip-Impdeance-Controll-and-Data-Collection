3ee6fb530d9d69266a7bf695b2f099d00def92d4
Tony Shilati <anthonyshilati@gmail.com>
2025-10-23 19:27:38 -0500

    Added the quadradure library as a reference and added config codefor it

--- Fingertip-Impedance-DAQ-and-Control/src/main.cpp
+++ Fingertip-Impedance-DAQ-and-Control/src/main.cpp
@@ -1,18 +1,27 @@
+#include <QuadEncoder.h>
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+QuadEncoder encoder1(3, 5, 7);  // ENC1 using pins 0 (A) and 1 (B)
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  /*
+   * Configure the pins for the quad encoder
+   */
+  encoder1.setInitConfig();   // load default settings
+  encoder1.init();            // initialize hardware encoder
+
+  pinMode(26, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+
+  digitalWrite(26, LOW);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, LOW);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
